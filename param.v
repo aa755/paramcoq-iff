cb8851396b0d35bc257b6cfd6225e8cef3b149d4
Abhishek Anand (@brixpro-home) <abhishek.anand.iitg@gmail.com>
2016-10-11 14:15:20 -0400

    added sig, which is need to enforce the property of relations.
    some comments about why reflection cannot be internally defined.
    Discussion on how to do typehood of the refleted terms.
    That typehood is need to ensure that we didn;t make a mistake

--- param.v
+++ param.v
@@ -12,18 +12,19 @@ Section VarSort.
 
 Variable V:Set.
 
-(* this type contains terms denoting Coq's sorts,
-which are Prop, Set, Type_i *)
-Variable S : Set.
-Variable translateS : S -> S.
+Section MaxUniv.
+
+(* max universe. 0 denotes [Set] *)
+Variable n : nat.
 
 Inductive Opid : Set :=
 (* x, which is var, we get for free *)
- | pSort : S -> Opid
+ | pSort : forall m, (PeanoNat.Nat.leb m n = true) -> Opid
  | pLam
  | pPi
-(* | pSig *)
+ | pSig
  | pApp
+ | projSig : bool (* true := fst, false := snd *) -> Opid
 (* pNat *)
 (* p0 *)
 (* pS *)
@@ -32,13 +33,42 @@ Inductive Opid : Set :=
 Definition OpBindings (c : Opid) 
     : list nat :=
   match c with
-  | pSort _ => [0] (* continuation lambda  *)
-  | pLam => [1] (* continuation lambda  *)
-  | pPi => [1] (* continuation lambda  *)
+  | pSort _ _ => [0]
+  | pLam => [0,1] (* contains type as well  *)
+  | pPi => [0,1]
+  | pSig => [0,1]
+  | projSig _ => [0]
   | pApp => [0;0]
   end.
 
-Let Term : Set := (@NTerm V Opid).
+
+End MaxUniv.
+Let Term : nat -> Set := (fun n => (@NTerm V (Opid n))).
+
+
+
+(* We can define the semantics of Term 1, by going as TermplateCoq.Term.
+Then, one would have to manually reflect it using the "Make Definition" command.
+Else, we can also just pretty print as a string, and dump the contents to a .v file. *)
+
+(*
+There is no way to give semantics where t:Term i is mapped to {T:Type | T}.
+Think about (oterm (pLam) (bterm [x] (vterm x))). We want it to map it to
+fun x => x. Suppose V:=string. There is no way to convert the string "x" to the binder x in fun x => x.
+There is not even a way to get a fresh binders. the "fresh" function is only available in Ltac.
+If there were such a function in Coq, we could cook up well-typed open terms, which is impossible.
+
+Perhaps thats why the "Make Definition" command of template-Coq is external
+*)
+
+(*
+Because there is no internal semantics function, we cannot use it to reduce the typehood of codes
+to the typehood of the semantics. It may be best to formalize the typehood.
+*)
+
+Fixpoint translate (n:nat) (t:Term n) : Term (S n).
+Abort.
+
 
 End VarSort.
 
