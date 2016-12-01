be9dde497ee9683ca7c808de1d98aff934af2119
Abhishek Anand (@brixpro-home) <abhishek.anand.iitg@gmail.com>
2016-11-30 19:06:31 -0500

    compiles with the new SquiggleEq library t

--- param.v
+++ param.v
@@ -6,7 +6,7 @@ Require Import SquiggleEq.UsefulTypes.
 Require Import SquiggleEq.list.
 Require Import SquiggleEq.LibTactics.
 Require Import SquiggleEq.tactics.
-Require Import SquiggleEq.lmap.
+Require Import SquiggleEq.AssociationList.
 
 Section VarSort.
 
