0f14a7945dbc549e434c1851929c4ead74292ecd
Abhishek Anand (@brixpro-home) <abhishek.anand.iitg@gmail.com>
2016-12-09 20:36:47 -0500

    proved the total and relIso for nonDep functions.
    dependence seems to cause problems

--- common.v
+++ common.v
@@ -102,3 +102,151 @@ forall a1 a2 b1 b2,
   R a1 b1
   -> R a2 b2
   -> (a1=a2 <-> b1=b2).
+
+Definition R_Pi {A1 A2 :Type} {A_R: A1 -> A2 -> Type}
+  {B1: A1 -> Type}
+  {B2: A2 -> Type} 
+  (B_R: forall {a1 a2}, A_R a1 a2 -> (B1 a1) -> (B2 a2) -> Type)
+  (f1: forall a, B1 a) (f2: forall a, B2 a) : Type
+  :=
+  forall a1 a2 (p: A_R a1 a2), B_R p (f1 a1) (f2 a2).
+
+Definition R_Fun {A1 A2 :Type} (A_R: A1 -> A2 -> Type)
+  {B1 B2: Type}
+  (B_R: B1 -> B2 -> Type)
+  (f1: A1->B1) (f2: A2->B2) : Type
+  :=
+  @R_Pi A1 A2 A_R (fun _ => B1) (fun _ => B2)
+  (fun _ _ _ => B_R) f1 f2.
+
+Lemma totalFun (A1 A2 :Type) (A_R: A1 -> A2 -> Type) 
+  {B1 B2: Type}
+  (B_R: B1 -> B2 -> Type)
+  (trp : TotalHeteroRel A_R)
+  (trb: TotalHeteroRel B_R)
+  (relISoA_R: relIso A_R)
+:
+  TotalHeteroRel (R_Fun A_R B_R).
+Proof.
+  split.
+- intros f1. apply snd in trp.
+  eexists.
+  Unshelve.
+    Focus 2.
+    intros a2. specialize (trp a2).
+     destruct trp as [a11 ar].
+    apply fst in trb.
+    specialize (trb (f1 a11)).
+    exact (projT1  trb).
+
+  simpl.
+  intros ? ? ?.
+  destruct (trp a2) as [a1r ar].
+  destruct (trb) as [b2 br].
+  simpl.
+  destruct (b2 (f1 a1r)). simpl.
+  pose proof (proj2 (relISoA_R _ _ _ _ p ar) eq_refl).
+  subst.
+  assumption.
+- intros f1. apply fst in trp.
+  eexists.
+  Unshelve.
+    Focus 2.
+    intros a2. specialize (trp a2).
+     destruct trp as [a11 ar].
+    apply snd in trb.
+    specialize (trb (f1 a11)).
+    exact (projT1  trb).
+
+  simpl.
+  intros a2 ? p.
+  destruct (trp a2) as [a1r ar].
+  destruct (trb) as [b2 br].
+  simpl.
+  destruct (br (f1 a1r)). simpl.
+  pose proof (proj1 (relISoA_R _ _ _ _ p ar) eq_refl).
+  subst.
+  assumption.
+Qed.
+
+Require Import Coq.Logic.FunctionalExtensionality.
+
+Lemma relIsoFun (A1 A2 :Type) (A_R: A1 -> A2 -> Type) 
+  {B1 B2: Type}
+  (B_R: B1 -> B2 -> Type)
+  (tra : TotalHeteroRel A_R)
+  (relISoB_R: relIso B_R)
+:
+  relIso (R_Fun A_R B_R).
+Proof.
+  intros f1 g1 f2 g2 H1r H2r.
+  unfold R_Fun, R_Pi in *.
+  split; intros Heq;subst; apply functional_extensionality.
+- intros a2.
+  destruct (snd tra a2) as [a1 a1r].
+  specialize (H2r _ _ a1r).
+  specialize (H1r _ _ a1r).
+  pose proof (proj1 (relISoB_R _ _ _ _ H2r H1r) eq_refl).
+  auto.
+- intros a2.
+  destruct (fst tra a2) as [a1 a1r].
+  specialize (H2r _ _ a1r).
+  specialize (H1r _ _ a1r).
+  pose proof (proj2 (relISoB_R _ _ _ _ H2r H1r) eq_refl).
+  auto.
+Qed.
+
+
+
+Lemma totalPi (A1 A2 :Type) (A_R: A1 -> A2 -> Type) 
+  (trp : TotalHeteroRel A_R) 
+  (B1: A1 -> Type) 
+  (B2: A2 -> Type) 
+  (B_R: forall a1 a2, A_R a1 a2 -> (B1 a1) -> (B2 a2) -> Type)
+  (trb: forall a1 a2 (p:A_R a1 a2), TotalHeteroRel (B_R _ _ p))
+  (relISoA_R: relIso A_R)
+  (relISoB_R: forall a1 a2 (a_r : A_R a1 a2), relIso (B_R a1 a2 a_r))
+:
+  TotalHeteroRel (R_Pi B_R).
+Proof.
+  split.
+- intros f1. apply snd in trp.
+  eexists.
+  Unshelve.
+    Focus 2.
+    intros a2. specialize (trp a2).
+     destruct trp as [a11 ar]. (* this step fails with TotalHeteroRelP *)
+    specialize (trb _ _ ar).
+    apply fst in trb.
+    specialize (trb (f1 a11)).
+    exact (projT1  trb).
+
+  simpl.
+  intros ? ? ?.
+  destruct (trp a2) as [a1r ar].
+  
+  destruct (trb a1r a2 ar) as [b2 br].
+  simpl.
+  destruct (b2 (f1 a1r)). simpl.
+  pose proof (proj2 (relISoA_R _ _ _ _ p ar) eq_refl).
+  subst.
+  assumption.
+  specialize (br x). destruct br.
+  destruct (trb a1 a2 p ) as [b22 br2].
+  specialize (b22 (f1 a1)).
+  destruct b22 as [xx dd].
+  specialize (br2 x).
+  destruct br2 as [xxx ddd].
+  
+  exrepnd.
+
+  specialize (b2 x0). destruct b2.
+  pose proof (proj2 (relISoB_R _ _ _ _ _ _ _ b b0) eq_refl). subst.
+  
+  
+  unfold relIso in relISoB_R.
+  eapply relISoB_R.
+  
+  
+Abort.
+
