ef616407e01b1c7a304c1d211bd5c6d8c64c5579
Abhishek Anand (@brixpro-home) <abhishek.anand.iitg@gmail.com>
2016-12-13 17:02:57 -0500

    closer to a decision now.

--- PIW.v
+++ PIW.v
@@ -164,6 +164,7 @@ Definition IWP_RP
 (BI_R : forall (a₁ : A₁) (a₂ : A₂) (a_R : A_R a₁ a₂) (H : B₁ a₁) (H0 : B₂ a₂),
         B_R a₁ a₂ a_R H H0 -> I_R (BI₁ a₁ H) (BI₂ a₂ H0))
  (H : I₁) (H0 : I₂) (i_R : I_R H H0) 
+(* extra *)
 (I_R_iso : oneToOne I_R) (*total Hetero not needed*)
 (A_R_tot : TotalHeteroRelP A_R) (* TotalHeteroRel implies TotalHeteroRelP *)
 (B_R_tot : forall (a₁ : A₁) (a₂ : A₂) (a_R : A_R a₁ a₂), TotalHeteroRelP (B_R _ _ a_R))
@@ -472,10 +473,19 @@ fix F (i : I) (i0 : IWP I A B AI BI i) {struct i0} : P i i0 :=
   end.
 
 
-(* Without Proof irrelevance, IWP_R is stronger than Prop_R2. It says 
+(*IWP_RP above was better because less extras were needed.
+In particular A_R iso and B_R iso and irrel was not needed.
+So, we should have the translation of [Prop] and [Type] be the same.
+However, we should separately translate inductives in [Type] and inductives in [Prop].
+Similarly, we should separately translate functions into Type and functions into Prop
+
+ Without Proof irrelevance, IWP_R is stronger than Prop_R2. It says 
 that the 2 proofs use the same constructors and those constructors "similar" arguments.
 Prop_R2 says nothing about the 2 proofs, besides their existence.
-However, with proof irrelevance, the difference is irrelevant.*)
+However, with proof irrelevance, the difference is irrelevant.
+However, as mentioned above, treating Prop specially, as in IWP_RP, results
+in much fewer extrans, and this riddance may snowball upstream
+and result in much stronger free theorems.*)
 Lemma IWP_R_total
 (I₁ I₂ : Type) (I_R : I₁ -> I₂ -> Type) (A₁ A₂ : Type) (A_R : A₁ -> A₂ -> Type)
 (B₁ : A₁ -> Type) (B₂ : A₂ -> Type)
@@ -588,7 +598,7 @@ Lemma IWP_R_iso
 (BI_R : forall (a₁ : A₁) (a₂ : A₂) (a_R : A_R a₁ a₂) (H : B₁ a₁) (H0 : B₂ a₂),
         B_R a₁ a₂ a_R H H0 -> I_R (BI₁ a₁ H) (BI₂ a₂ H0))
  (H : I₁) (H0 : I₂) (i_R : I_R H H0)
-(* extra*)
+(* extra
 (I_R_iso : oneToOne I_R) (*total Hetero not needed*)
 (irrel : rellIrrUptoEq I_R)
 (A_R_tot : TotalHeteroRel A_R)
@@ -597,18 +607,21 @@ Lemma IWP_R_iso
 (B_R_tot : forall (a₁ : A₁) (a₂ : A₂) (a_R : A_R a₁ a₂), TotalHeteroRel (B_R _ _ a_R))
 (B_R_iso : forall (a₁ : A₁) (a₂ : A₂) (a_R : A_R a₁ a₂), oneToOne (B_R _ _ a_R))
 (B_R_irrel : forall (a₁ : A₁) (a₂ : A₂) (a_R : A_R a₁ a₂), rellIrrUptoEq (B_R _ _ a_R))
-
+*)
 :
   oneToOne (IWP_R _ _ I_R _ _ A_R _ _ B_R _ _ AI_R _ _ BI_R _ _ i_R).
 Proof using.
+  intros ? ? ? ? ? ?; split; intros ?; apply proof_irrelevance.
+Qed.
 
+(*
   rename H into i₁.
   rename H0 into i₂. intros l1 l2 r1 r2 ir1 ir2. split.
 - revert l2 r2 ir2. induction ir1 as [ ? ? ? ? ? ? Hind] using IWP_R_rect.
   intros.
   subst.
   inversion ir2. subst.
-(*
+  Fail pose proof (proj1 (A_R_iso _ _ _ _ a_R a_R0) eq_refl) as heq.
   pose proof (proj1 (A_R_iso _ _ _ _ a_R a_R0) eq_refl) as heq.
   symmetry in heq.
   subst.
@@ -629,8 +642,9 @@ Proof using.
   subst.
   apply X2.
 - (* the other side will be similar *)
-*)
+
 Abort.
+*)
 
 Require Import Coq.Logic.JMeq.
 Require Import Coq.Program.Equality.
@@ -651,6 +665,9 @@ Lemma IWP_R_irrel
 (BI_R : forall (a₁ : A₁) (a₂ : A₂) (a_R : A_R a₁ a₂) (H : B₁ a₁) (H0 : B₂ a₂),
         B_R a₁ a₂ a_R H H0 -> I_R (BI₁ a₁ H) (BI₂ a₂ H0))
  (H : I₁) (H0 : I₂) (i_R : I_R H H0)
+(* extra
+(A_R_irrel : rellIrrUptoEq A_R)
+*)
 :
   rellIrrUptoEq (IWP_R _ _ I_R _ _ A_R _ _ B_R _ _ AI_R _ _ BI_R _ _ i_R).
 Proof using.
