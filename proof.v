6c0c68d137a750a9a1738ed957903c01e8d9192b
Abhishek Anand (@brixpro-home) <abhishek.anand.iitg@gmail.com>
2017-01-07 18:10:18 -0500

    started the proof file

---
+++ proof.v
@@ -0,0 +1,19 @@
+Require Import Coq.Classes.DecidableClass.
+Require Import Coq.Lists.List.
+Require Import Coq.Bool.Bool.
+Require Import SquiggleEq.export.
+Require Import SquiggleEq.UsefulTypes.
+Require Import SquiggleEq.list.
+Require Import SquiggleEq.LibTactics.
+Require Import SquiggleEq.tactics.
+Require Import SquiggleEq.AssociationList.
+Require Import ExtLib.Structures.Monads.
+Require Import templateCoqMisc.
+Require Import Template.Template.
+Require Import Template.Ast.
+
+
+Require Import Program.
+Open Scope program_scope.
+
+
