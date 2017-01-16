6d39f2f3d7ecfbc4670d46a865118fde4ba268c9
Abhishek Anand (@brixpro-home) <abhishek.anand.iitg@gmail.com>
2017-01-16 18:35:22 -0500

    isoRel modes doesn't work yet because i_RR isn't total yet

--- test-suite/indFunArg.v
+++ test-suite/indFunArg.v
@@ -1,7 +1,3 @@
-(*
-abhishek@brixpro:~/parametricity/reflective-paramcoq/test-suite$ ./coqid.sh indFunArg
-*)
-
 Require Import ReflParam.common.
 Require Import ReflParam.templateCoqMisc.
 Require Import String.
@@ -25,8 +21,7 @@ Inductive NatLike (A:Set) (C: A-> Set): Set :=
  | SS2 :  forall (d:forall a:A,NatLike A C),
        NatLike A C.
 
-Let mode := false.
-Run TemplateProgram (genParamInd [] mode true true true "Top.indFunArg.NatLike").
+Run TemplateProgram (genParamInd [] false true true false "Top.indFunArg.NatLike").
 
 (*
 (fix
@@ -62,4 +57,51 @@ Top_NatLike_RR0_paramConstr_0_paramInv is defined
           A_R a a₂ -> Top_NatLike_RR0 A A₂ A_R C C₂ C_R (d a) (d₂ a₂)) => 
  existT d_R I)
 Top_NatLike_RR0_paramConstr_0 is defined
+*)
+
+(* Fails in False mode because the IWT_RR is not a goodrel yet.
+Run TemplateProgram (genParamInd [] false true true false "Top.indFunArg.NatLike").
+(*
+(fun (A A₂ : Set) (A_R : Trecord.BestRel A A₂) (C : A -> Set)
+   (C₂ : A₂ -> Set)
+   (C_R : forall (H : A) (H0 : A₂),
+          Trecord.BestR A_R H H0 -> Trecord.BestRel (C H) (C₂ H0))
+   (d : A -> NatLike A C) (d₂ : A₂ -> NatLike A₂ C₂)
+   (d_R : Trecord.BestR
+            (PiTSummary A A₂ A_R (fun _ : A => NatLike A C)
+               (fun _ : A₂ => NatLike A₂ C₂)
+               (fun (a : A) (a₂ : A₂) (_ : Trecord.BestR A_R a a₂) =>
+                Top_indFunArg_NatLike_RR0 A A₂ A_R C C₂ C_R)) d d₂) =>
+ existT
+   (fun
+      _ : Trecord.BestR
+            (PiTSummary A A₂ A_R (fun _ : A => NatLike A C)
+               (fun _ : A₂ => NatLike A₂ C₂)
+               (fun (a : A) (a₂ : A₂) (_ : Trecord.BestR A_R a a₂) =>
+                Top_indFunArg_NatLike_RR0 A A₂ A_R C C₂ C_R)) d d₂ => True)
+   d_R I)
+File "./indFunArg.v", line 25, characters 0-82:
+Error:
+In environment
+A : Set
+A₂ : Set
+A_R : Trecord.BestRel A A₂
+C : A -> Set
+C₂ : A₂ -> Set
+C_R :
+forall (H : A) (H0 : A₂),
+Trecord.BestR A_R H H0 -> Trecord.BestRel (C H) (C₂ H0)
+d : A -> NatLike A C
+d₂ : A₂ -> NatLike A₂ C₂
+a : A
+a₂ : A₂
+b : Trecord.BestR A_R a a₂
+The term "Top_indFunArg_NatLike_RR0 A A₂ A_R C C₂ C_R" has type
+ "NatLike A C -> NatLike A₂ C₂ -> Prop" while it is expected to have type
+ "Trecord.GoodRel Trecord.allProps (NatLike A C) (NatLike A₂ C₂)"
+(cannot unify "NatLike A C -> NatLike A₂ C₂ -> Prop" and
+"Trecord.GoodRel Trecord.allProps ((fun _ : A => NatLike A C) a)
+   ((fun _ : A₂ => NatLike A₂ C₂) a₂)").
+
+*)
 *)
