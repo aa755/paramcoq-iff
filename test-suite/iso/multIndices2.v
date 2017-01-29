6f07edc710970281ce978d233a0270ea7edabf05
Abhishek Anand (@brixpro-home) <abhishek.anand.iitg@gmail.com>
2017-01-29 18:07:18 -0500

    finished the proof manually. it seems each index can be written
    separately as a sequence of transports.

--- test-suite/iso/multIndices2.v
+++ test-suite/iso/multIndices2.v
@@ -22,9 +22,46 @@ Run TemplateProgram (genParamInd [] true true true "Top.multIndices2.multInd").
 
 Require Import ReflParam.Trecord.
 
-Run TemplateProgram (genParamIndTot [] true true "Top.multIndices2.multInd").
 
-Definition multIndices2Tot :=
+Definition multIndices2Tot : forall
+(A A₂ : Set) 
+                                     (A_R : BestRel A A₂) 
+                                     (I I₂ : Set) 
+                                     (I_R : BestRel I I₂) 
+                                     (B : I -> Set) 
+                                     (B₂ : I₂ -> Set)
+                                     (B_R : forall (H : I) (H0 : I₂),
+                                            BestR I_R H H0 ->
+                                            BestRel (B H) (B₂ H0))
+                                     (f : A -> I) 
+                                     (f₂ : A₂ -> I₂)
+                                     (f_R : BestR
+                                              (PiTSummary A A₂ A_R
+                                                 (fun _ : A => I)
+                                                 (fun _ : A₂ => I₂)
+                                                 (fun 
+                                                 (H : A) 
+                                                 (H0 : A₂)
+                                                 (_ : BestR A_R H H0) => I_R))
+                                              f f₂) 
+                                     (g : forall i : I, B i)
+                                     (g₂ : forall i₂ : I₂, B₂ i₂)
+                                     (g_R : BestR
+                                              (PiTSummary I I₂ I_R
+                                                 (fun i : I => B i)
+                                                 (fun i₂ : I₂ => B₂ i₂)
+                                                 (fun 
+                                                 (i : I) 
+                                                 (i₂ : I₂)
+                                                 (i_R : BestR I_R i i₂) =>
+                                                 B_R i i₂ i_R)) g g₂) 
+                                     (i : I) (i₂ : I₂) 
+                                     (i_R : BestR I_R i i₂) 
+                                     (b : B i) (b₂ : B₂ i₂)
+                                     (b_R : BestR (B_R i i₂ i_R) b b₂)
+                                     (H : multInd A I B f g i b),
+   multInd A₂ I₂ B₂ f₂ g₂ i₂ b₂ .
+refine(
 (fix
  Top_multIndices2_multInd_pmtcty_RR0 (A A₂ : Set) 
                                      (A_R : BestRel A A₂) 
@@ -71,10 +108,26 @@ Definition multIndices2Tot :=
    with
    | mlind _ _ _ _ _ a =>
        fun (i₂0 : I₂) (b₂0 : B₂ i₂0) (i_R0 : BestR I_R (f a) i₂0)
-         (_ : BestR (B_R (f a) i₂0 i_R0) (g (f a)) b₂0) =>
+         (br : BestR (B_R (f a) i₂0 i_R0) (g (f a)) b₂0) =>
        let a₂ := BestTot12 A_R a in
-       let a_R := BestTot12R A_R a in mlind A₂ I₂ B₂ f₂ g₂ a₂
-   end i₂ b₂ i_R b_R).
+       let a_R := BestTot12R A_R a in 
+       let c2 := (mlind A₂ I₂ B₂ f₂ g₂ a₂) in _
+   end i₂ b₂ i_R b_R)).
+   set (peq := @BestOne12 I I₂ I_R (f a) i₂0 (f₂ a₂) i_R0 (f_R a a₂ a_R)).
+   set (c22 := @transport _ _ _ 
+      (fun i2:I₂ => multInd A₂ I₂ B₂ f₂ g₂ i2 (g₂ i2)) 
+          peq c2).
+   simpl in c22.
+   assert ((g₂ i₂0) = b₂0) as peq2.
+   - apply (@BestOne12 _ _ (B_R (f a) i₂0 i_R0)) with (a0:=(g (f a))).
+     + apply br.
+     + apply g_R.
+   -  apply (transport peq2). exact c22.
+Defined.
+
+(*   
+   Run TemplateProgram (genParamIndTot [] true true "Top.multIndices2.multInd").
+*)
 
 
 
