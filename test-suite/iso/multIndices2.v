768a1a8df5ecba10eeff9afb515e102fe792e218
Abhishek Anand (@brixpro-home) <abhishek.anand.iitg@gmail.com>
2017-01-29 22:24:51 -0500

    more comments. it seems that the main complication is not from dep indices

--- test-suite/iso/multIndices2.v
+++ test-suite/iso/multIndices2.v
@@ -119,7 +119,8 @@ refine(
    if pair = (ch, ih:IH)::tl then 
    @BestOne12 IH (tprime IH) (translate IH) ch ih (ptime ch) ir0 (translate ch)
    in transport 
-   in tl, do some substitutions
+   in tl, do some substitutions: replace (tprime ch) by (tprime ih).
+   replace [ch] by (vrel (fst ih))
     *)
    set (peq := @BestOne12 I I₂ I_R (f a) i₂0 
 (* so far this exactly matches the type of br above *)
@@ -129,12 +130,21 @@ refine(
            (g₂ i2(*we had to convert this from (f₂ a₂) in c2 *)))
           peq c2).
    simpl in c22.
-   
+  (*
+  assert (g₂ i₂0 = b₂0).
+  apply (@BestOne12 (B (f a)) (B₂ i₂0) (B_R (f a) i₂0 i_R0) (g (f a)) b₂0).
+  apply br.
+  simpl in g_R.
+  apply g_R.
+  *)
+  
   set (peq2 :=
 @BestOne12 (B (f a)) (B₂ i₂0) (B_R (f a) i₂0 i_R0) (g (f a)) b₂0 
 (* so far this exactly matches the type of br above *)
            (g₂ i₂0) br (g_R (f a) i₂0 (*we had to convert this from (f₂ a₂) in c2 *) 
-           i_R0 (* [f a] was replaced with i_R0  *) )).
+           i_R0 (* [f a] was replaced with i_R0. it seems that this will
+           be needed even if the second index was not dependent.  *) )).
+           
   exact (@transport (B₂ i₂0) (g₂ i₂0) b₂0 (multInd A₂ I₂ B₂ f₂ g₂ i₂0) peq2 c22).
 Defined.
 
