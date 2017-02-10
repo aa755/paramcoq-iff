0db7248782cfb876cae247c6af3e8fef6967d9b1
Abhishek Anand (@brixpro-home) <abhishek.anand.iitg@gmail.com>
2017-02-09 22:13:52 -0500

    defined eval. need exists in Prop for squiggle.

--- test-suite/iso/squiggle.v
+++ test-suite/iso/squiggle.v
@@ -17,6 +17,15 @@ Require Import ReflParam.Trecord.
 Inductive sigs (A : Set) (P : A -> Prop) : Set :=
  existss : forall x : A, P x -> sigs A P.
  *)
+Inductive Op : Set :=
+ | lam
+ | app 
+ | num (n : nat)
+(* | primRec 
+  
+  Variable primRec: Tm (* num*) ->Tm (* base case*) ->Tm (* rec case *) -> Tm.
+ *)
+.
 
 Set Imlicit Arguments.
 
@@ -28,14 +37,37 @@ Definition beq (b1 b2 : bool) := eqs bool b1 b2.
 Infix "≡" := beq (at level 80).
  *)
 Section Squiggle.
-  Variable V:Set.
+  (* Variable V:Set. This interface is too abstract for exposing V *)
   Variable Tm:Set.
   Variable BTm:Set.
-  Variable app: Tm -> Tm -> Tm.
-  Variable lam: BTm -> Tm.
-  Variable num: nat -> Tm.
-  (*
-  Variable primRec: Tm (* num*) ->Tm (* base case*) ->Tm (* rec case *) -> Tm.
-  *)
+  Variable mkTerm : Op -> list (Tm + BTm) -> option Tm.
+  Variable elimTerm : Tm -> option (Op* list (Tm+BTm)).
   Variable applyBtm: BTm -> Tm -> Tm.
+
+Fixpoint evaln (n:nat) (t:Tm): option Tm :=
+match n with
+|0%nat => None
+| S n =>
+  match (elimTerm t)
+  with
+  | Some (lam, _)
+  | Some (num _, _) => Some t
+  | Some (app, [inl f; inl a]) =>
+    match evaln n f, evaln n a with
+    | Some f, Some a =>
+      match (elimTerm f) with
+      | Some (lam, [inr bt]) =>
+        Some (applyBtm bt a)
+      | _ => None
+      end
+    | _,_ => None
+    end        
+  | _ => None                    
+  end
+end.
+
+(*
+Fixpoint preOrd (n:nat) (t:Tm): option Tm :=
+*)  
+
 End Squiggle.
