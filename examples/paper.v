b7b28d16ce607c3a11c3f7dd9fee977298b80f45
Abhishek Anand (@brixpro-home) <abhishek.anand.iitg@gmail.com>
2017-02-20 08:14:30 -0500

    added the obEq example from yesterday. added poly ID for intro

--- examples/paper.v
+++ examples/paper.v
@@ -648,3 +648,100 @@ Definition projRel (A A' : Set) (A_RG : GoodRel A A') : A -> A' -> Prop.
 Abort.
 
 Inductive  Monad : forall (A:Set), Set := ret : Monad nat.
+
+Parametricity Recursive ID.
+Print Coq_o_Init_o_Datatypes_o_ID_R.
+Definition idR :=
+λ f f' : ∀ A : Type, A → A,
+∀ (A₁ A₂ : Type) (A_R : A₁ → A₂ → Type) (a : A₁) (a' : A₂),
+A_R a a' → A_R (f A₁ a) (f' A₂ a').
+
+Inductive eqs {A : Set} (x : A) : forall (a:A), Prop :=  
+  eq_refls : eqs x x.
+
+Inductive option (A : Set) : Set :=  Some : A -> option A | None : option A.
+Arguments Some {A} _.
+Arguments None {A}.
+
+Infix "=" := eqs : type_scope.
+Notation True := (true=true).
+Notation False := (false=true).
+
+Definition isNone {A:Set} (oa: option A) :=
+  match oa with
+  | Some _ => false
+  | None => true
+  end.
+Infix "+" := sum:type_scope.
+(*
+Definition beq (b1 b2 : bool) := eqs bool b1 b2.
+ *)
+
+
+Open Scope nat_scope.
+
+Section Squiggle4.
+(* Variable V:Set. This interface is too abstract for exposing V *)
+Variables (Tm BTm : Set).
+Variable applyBtm: BTm -> Tm -> Tm.
+
+Inductive TmKind :=
+| elam (bt: BTm) 
+| eapp (f a: Tm) 
+| enum (n: nat)
+| evar.
+
+Variable tmKind:  Tm -> TmKind.
+
+Section eval.
+
+Variable evaln: nat -> Tm -> option Tm.
+
+(* just this would be an example. However, because it is not recursive,
+ even tauto may be able to prove it. Even if we only show this on paper,
+we should have a more complex (recursively defined undefined relation)
+in the appendix *)
+Definition divergesIff (tl tr:Tm) : Prop :=
+ (forall (nsteps:nat), (isNone (evaln nsteps tl)) = true) 
+ <-> (forall (nsteps:nat), (isNone (evaln nsteps tr)) = true).
+
+ (* need to eliminate the oneOne of Prop inductives and use PI *)
+Fixpoint obsEq (k:nat) (tl tr:Tm): Prop :=
+divergesIff tl tr /\ forall (nsteps:nat), 
+match k with | O => True | S k =>
+ match evaln nsteps tl, evaln nsteps tr with
+ |Some vl, Some vr => 
+  match tmKind vl, tmKind vr with
+  | enum nl , enum nr => nl = nr
+  | elam btl , elam btr => forall (ta: Tm), 
+    obsEq k (applyBtm btl ta) (applyBtm btr ta)
+  | eapp fl al , eapp fr ar => 
+      obsEq k fl fr /\ obsEq k al ar
+  | _,_ => False
+  end
+ |_, _  => True
+ end
+end.
+
+End eval.
+
+Fixpoint evaln (n:nat) (t:Tm): option Tm :=
+match n with
+| O => None | S n => 
+  match (tmKind t) with
+  | evar | elam _ | enum _ => Some t
+  | eapp f a =>
+    match evaln n f, evaln n a with
+    | Some f, Some a =>
+      match (tmKind f) with
+      | elam bt => Some (applyBtm bt a)
+      | _ => None
+      end
+    | _,_ => None
+    end
+  end
+end.
+
+End Squiggle4.
+
+
