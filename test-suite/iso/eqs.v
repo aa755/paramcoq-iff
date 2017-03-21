71901ad1b4de022f92d642d35e495f10655cae60
Abhishek Anand (@brixpro-home) <abhishek.anand.iitg@gmail.com>
2017-03-21 17:53:01 -0400

    added more test cases: isorel translation of match on proofs

--- test-suite/iso/eqs.v
+++ test-suite/iso/eqs.v
@@ -19,3 +19,16 @@ Run TemplateProgram (genParamIndPropAll [] "Top.eqs.eqs").
 Run TemplateProgram (mkIndEnv "indTransEnv" ["Top.eqs.eqs"]).
 
 (* Run TemplateProgram (genWrappers indTransEnv). *)
+
+
+Definition eqs_recs := 
+fun (A : Set) (x : A) (P : A -> Set) (f : P x) (a : A) (e : eqs A x a) =>
+match e in (eqs _ _ a0) return (P a0) with
+| eq_refls _ _ => f
+end.
+
+Run TemplateProgram (genParam indTransEnv true true "Top.eqs.eqs_recs").
+Module AnyRel.
+Run TemplateProgram (genParam indTransEnv false true "Top.eqs.eqs_recs").
+End AnyRel.
+
