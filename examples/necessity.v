d26701b479ce68e5b71572ebaa3dbdd307f1e13c
Abhishek Anand (@ CU desktop) <abhishek.anand.iitg@gmail.com>
2017-04-18 16:41:23 -0400

    it seems a different structure is needed

--- examples/necessity.v
+++ examples/necessity.v
@@ -1,14 +1,16 @@
-Definition ExistsTrue (T:Type) :=  exists t:T, True.
+Definition ExistsB (b:bool) (T:Type) (p: T -> bool):=  exists t:T, p t = b.
   
-Definition fType := forall (T:Set), ExistsTrue T.
+Definition fType := forall (T:Set) (p: T -> bool), ExistsB true T p * ExistsB false T p.
 
 
 (* This is logicall equivalent to the AnyRel translation of ExistsTrue. We
 have simplified it a bit, just for better readability.*)
-Inductive ExistsTrueR {T1 T2: Set} (R: T1 -> T2 -> Prop)
-  : forall (p1 : ExistsTrue T1) (p2 : ExistsTrue T2), Prop :=
-| exTrueR : forall (t1: T1) (t2: T2) p1 p2,
-  R t1 t2 ->  ExistsTrueR R (ex_intro _ t1 p1) (ex_intro _ t2 p2).
+Inductive ExistsBR (b:bool) {T1 T2: Set} (R: T1 -> T2 -> Prop)
+          {p1: T1-> bool} {p2: T2-> bool} (pR : forall t1 t2, R t1 t2 -> p1 t1 = p2 t2)
+  : forall (dp1 : ExistsB b T1 p1) (dp2 : ExistsB b T2 p2), Prop :=
+| exTrueR : forall (t1: T1) (t2: T2) dp1 dp2,
+    R t1 t2 ->
+    ExistsBR b R pR (ex_intro _ t1 dp1) (ex_intro _ t2 dp2).
 
 (*
 Definition IffProps {A B : Prop} (_: A -> B -> Prop)  := (A <-> B).
@@ -17,9 +19,11 @@ Definition IffProps {A B : Prop} (_: A -> B -> Prop)  := (A <-> B).
 Definition TotalHeteroRelHalf {T1 T2 : Type} (R: T1 -> T2 -> Prop) : Type :=
   (forall (t1:T1), ex (R t1)).
 
-Definition ExistsTrueRIso {T1 T2: Set} (R: T1 -> T2 -> Prop)
-           (p1 : ExistsTrue T1) (p2 : ExistsTrue T2) :=
-  ExistsTrueR R p1 p2 /\ (ExistsTrue T1 <->  ExistsTrue T2).
+Definition ExistsTrueRIso (b:bool) {T1 T2: Set} (R: T1 -> T2 -> Prop)
+           {p1: T1-> bool} {p2: T2-> bool} (pR : forall t1 t2, R t1 t2 -> p1 t1 = p2 t2)
+           (dp1 : ExistsB b T1 p1) (dp2 : ExistsB b T2 p2)
+  :=
+  ExistsBR b R pR dp1 dp2 /\ (ExistsB b T1 p1 <->  ExistsB b T2 p2).
 
 Section TotalityNec.
   Variable f:fType.
@@ -28,14 +32,22 @@ Section TotalityNec.
 we assume the result and prove the assumptions that are
 supposed to be necessary. This is the reverse direction of what we did in Sec 3.
 of the paper. *)
-  Hypothesis fR: forall (T1 T2: Set) (R: T1 -> T2 -> Prop), ExistsTrueRIso R (f T1) (f T2).
+  Hypothesis fR: forall  {T1 T2: Set} (R: T1 -> T2 -> Prop)
+                    {p1: T1-> bool} {p2: T2-> bool} (pR : forall t1 t2, R t1 t2 -> p1 t1 = p2 t2)
+    , ExistsTrueRIso true R pR (fst (f T1 p1)) (fst (f T2 p2))
+  /\ ExistsTrueRIso false R pR (snd (f T1 p1)) (snd (f T2 p2)).
 
   (* we only show one side of the proof. *)
-  Lemma totalAssumptionNecessary (T1 T2: Set) (R: T1 -> T2 -> Prop) :
+  Lemma totalAssumptionNecessary (T1 T2: Set) (R: T1 -> T2 -> Prop) 
+     {p1: T1-> bool} {p2: T2-> bool} (pR : forall t1 t2, R t1 t2 -> p1 t1 = p2 t2) :
     TotalHeteroRelHalf R.
   Proof using.
-    intros ?. specialize (fR T1 T2 R). destruct fR as [fRa fRIff]. clear fR.
-    apply proj1 in fRIff. specialize (fRIff (ex_intro _ t1 I)).
-    destruct fRIff. exists x. destruct fRa.
+    intros ?. specialize (fR T1 T2 R _ _ pR ).
+    apply proj1 in fR.
+    remember (p1 t1) as pt1. symmetry in Heqpt1. destruct pt1.
+-    destruct fR as [fRa fRIff]. clear fR.
+     apply proj1 in fRIff.
+     specialize (fRIff (ex_intro _ t1 Heqpt1)).
+     destruct fRIff. exists x. inversion fRa.
   Abort.
     
