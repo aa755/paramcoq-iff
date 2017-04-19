19050c138b5f218c912f2fee0febc9b677112b41
Abhishek Anand (@brixpro-home) <abhishek.anand.iitg@gmail.com>
2017-04-19 17:41:51 -0400

    minor fix, added FAQ

--- examples/necessity.v
+++ examples/necessity.v
@@ -2,13 +2,14 @@
 Definition left_identity {S T : Type} (f: S -> T) (g: T-> S): Prop :=
   forall s: S , (g (f s)) = s.
 
-(** isomorphism w.r.t. the structure (T,=) *)
+(** isomorphism w.r.t. the structure (T,=). Also, see isomorphic.v in the same directory for
+  a characterization in terms of Total and OneToOne. *)
 Definition isomorphic (A B : Set) : Prop :=
   ex (fun f: A->B => ex (fun g: B->A =>  left_identity f g /\ left_identity g f)).
 
 Section Necessity.
   Variables (A1 A2 : Set).
-(** Suppose that there is a tool T than can, for ANY closed [P] of type [Set -> Prop], 
+(** Suppose that there is a tool T than can, for ANY [P] of type [Set -> Prop], 
 produce a proof of [P A1 <-> P A2]. 
 In the style of reverse mathematics (https://en.wikipedia.org/wiki/Reverse_mathematics), 
 to prove that the assumption of isomorphism was necessary for the tool,
