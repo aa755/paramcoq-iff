ef65512ad9083f435bd90e577b151694213fb41c
Abhishek Anand (@brixpro-home) <abhishek.anand.iitg@gmail.com>
2017-04-18 23:42:23 -0400

    minor elaboration.

--- examples/necessity.v
+++ examples/necessity.v
@@ -9,10 +9,14 @@ Definition isomorphic (A B : Set) : Prop :=
 Section Necessity.
   Variables (A1 A2 : Set).
 (** Suppose that there is a tool T than can, for ANY closed [P] of type [Set -> Prop], 
-produce a proof of [P A1 <-> P A2]. Now we need to prove [isomorphic A1 A2].
+produce a proof of [P A1 <-> P A2]. 
+In the style of reverse mathematics (https://en.wikipedia.org/wiki/Reverse_mathematics), 
+to prove that the assumption of isomorphism was necessary for the tool,
+we assume that the tool always succeeds, and prove [isomorphic A1 A2].
 
 We instantiate the tool T for [P:= (isomorphic A1)] to get a proof of 
-[isomorphic A1 A1 <-> isomorphic A1 A2], which implies [isomorphic A1 A2]
+[isomorphic A1 A1 <-> isomorphic A1 A2], which implies [isomorphic A1 A2].
+Qed.
  *)
   Check ((isomorphic A1) : Set -> Prop).
 End Necessity.
