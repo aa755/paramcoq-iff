b2039bcd10607e64b9d46bec3499b840e651dcbe
Abhishek Anand (@brixpro-home) <abhishek.anand.iitg@gmail.com>
2017-04-20 18:37:14 -0400

    minor fixes

--- examples/necessity.v
+++ examples/necessity.v
@@ -30,7 +30,7 @@ propositional constructs in Coq and ask whether those were necessary.
 For example, we for indexed inductive propositions to behave uniformly, we assume
 that the relation for the index types is one to one.
 Was this assumption necessary?
-We show that it is necessary at least for the equality types, which are indexed inductive types:
+We show that it is necessary at least for the equality, which is an indexed inductive proposition:
  *)
 
   Print Coq.Init.Logic.eq.
@@ -42,29 +42,31 @@ We show that it is necessary at least for the equality types, which are indexed 
   (* = is a notation for the above proposition *)
   Locate "=".
   
-  (**
+(**
  For eq to behave uniformly, we must prove the following: 
 *)
-  Lemma eqUnif (A1 A2:Set) (AR : A1 -> A2 -> Prop) (* some assumption about AR *)
-        (l1: A1) (l2: A2) (lR : AR l1 l2)
-        (r1: A1) (r2: A2) (rR : AR r1 r2) :
+  Lemma eqUnif (A1 A2:Set) (AR : A1 -> A2 -> Prop) (* some assumption about AR *) :
+    forall   (l1: A1) (l2: A2) (lR : AR l1 l2)
+        (r1: A1) (r2: A2) (rR : AR r1 r2),
     (l1 =  r1) <-> (l2 = r2).
   Abort.
 
 (** 
 The above is precisely the definition of one to one!  
 In fact, we obtained our definition of one to one by just 
-extracting what we had to prove for the uniformity of the equality type. 
+copying what we had to prove for the uniformity of the equality type. 
 
-What is surprising is that it sufficies even for other indexed inductive 
-types and propositions, and that we can compositionally build the proof 
+What is surprising is that it sufficies even for other indexed inductive propositions, 
+and that we can compositionally build the proof 
 of one-to-one-ness (and totality) for composite types (the index type may be a composite type).
  *)
 
   Require Import Coq.Unicode.Utf8.
 
 (** our current definition of one to one is slightly different but logically equivalent. *)
-Definition OneToOne {A B : Set} (R : A → B → Prop) : Prop := (∀ (a:A) (b1 b2: B), R a b1 → R a b2 → b1=b2) ∧ (∀ (b:B) (a1 a2: A), R a1 b → R a2 b → a1=a2).
+  Definition OneToOne {A B : Set} (R : A → B → Prop) : Prop
+    := (∀ (a:A) (b1 b2: B), R a b1 → R a b2 → b1=b2)
+       ∧ (∀ (b:B) (a1 a2: A), R a1 b → R a2 b → a1=a2).
 
 (** The above is logically equivalent to: *)
 
