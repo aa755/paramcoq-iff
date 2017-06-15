18cfdfdf4c0f41e81a9c31d221e1b2f4ca4c891c
Abhishek Anand (@ VM in vaio) <abhishek.anand.iitg@gmail.com>
2017-06-15 12:45:33 -0400

    timing the correct answr

--- test-suite/iso/squiggle4TestMinrel.v
+++ test-suite/iso/squiggle4TestMinrel.v
@@ -31,13 +31,12 @@ let A_R := (@Build_GoodRel allProps _ _ Ra pta poa) in
 let B_R := (@Build_GoodRel allProps _ _ Rb ptb pob) in
 Top_squiggle4_obsEq_pmtcty_RR _ _ A_R _ _ B_R
 ).
-Detect (
+Time Detect (
 fun (pob : oneToOne Rb) =>
 let A_R := (@Build_GoodRel allProps _ _ Ra pta poa) in
 let B_R := (@Build_GoodRel allProps _ _ Rb ptb pob) in
 Top_squiggle4_obsEq_pmtcty_RR _ _ A_R _ _ B_R
 ).
-
 End Test.
 Lemma obsEqExistsAOneFreeImpl  : existsAOneFreeImpl2
   (Top_squiggle4_obsEq_pmtcty_RR).
@@ -46,10 +45,11 @@ Proof.
   eexists.
   intros. simpl in *.
   set (fvv:= Top_squiggle4_obsEq_pmtcty_RR _ _ A_R _ _ B_R).
+  
   simpl in *.
-  lazy in fvv.
-  reflexivity.
-Defined.
+  Time lazy in fvv.
+Abort.
+
 
 (*
 Lemma dependsOnlyOnTotdivergesIff  : existsAOneFreeImpl
